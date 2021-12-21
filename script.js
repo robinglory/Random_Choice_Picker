d9f00aef94892566ee2cb9ccb87f64d2443c394b
Glory <koyannaingkyawtint@gmail.com>
2021-12-21 23:23:44 +0630

    Adding Js File part 2

--- script.js
+++ script.js
@@ -1,27 +1,68 @@
-var tagsEl = document.getElementById('tags');
-var textarea = document.getElementById('textarea');
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
 
 textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
-   createTags(e.target.value)
+    createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
 })
 
 function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    
+    tagsEl.innerHTML = ''
 
-   let tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
-
-   tagsEl.innerHTML = ''
-
-   tags.forEach(tag => {
-      const tagEl = document.createElement('span')
-      tagEl.classList.add('tag')
-      tagEl.innerHTML = tag
-      tagsEl.appendChild(tagEl)
-   })
-
-
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
 }
 
+function randomSelect() {
+    const times = 30
 
- 
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+	
+	if (randomTag !== undefined) {
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+	}
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('tag-highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('tag-highlight')}
